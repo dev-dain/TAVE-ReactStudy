4065a36c9bf3ffbdfc03ea89444de928a46d4889
Dain Kim <dev.dain.k@gmail.com>
2021-03-17 00:35:52 +0900

    TAVE 1주차 스터디 분량

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function getTitle(title) {
+  return title;
+}
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
 
 function App() {
+  const title = [1, 2, 3, 4, 5];
+
+  // JSX에서 자바스크립트는 { } 안에 쓸 수 있음
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Hello {getTitle('React!')} {title}
+        {/* 그냥 JS 배열 자체를 넣으면 배열 안의 요소가 다 붙어서 나옴*/}
+      </h1>
+      <Search />
+
+      <hr />
+      <List />
     </div>
   );
 }
+
+function Search() {
+  // <React.Fragment>를 쓰면 같은 레벨에서 사용 가능
+  // return은 루트 컴포넌트 안에 하위 태그들을 넣어야지
+  // 루트 컴포넌트가 2개 이상 있으면 X
+  // event.target(input#search).value로 input값 접근 가능
+  const handleChange = event => console.log(event.target.value);
+
+  // const handleChange = e => {
+  //   const input = document.querySelector('input');
+  //   console.log(input.value);
+  // }
+
+  //input에 onchange 프로퍼티가 원래 있긴 한데
+  //onchange=javascript function 이런 식으로 씀
+  return (
+    <React.Fragment>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange}/>
+    </React.Fragment>
+    // <div>
+    //   <label htmlFor="search">Search: </label>
+    //   <input id="search" type="text" />
+    // </div>
+  );
+}
+
+// 컴포넌트 정의부
+const List = () =>
+  /* key prop이 있어야 함. 콘솔을 살펴볼 것*/
+  list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; //ReactDOM 사용을 위해 import
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// ReactDOM.render() 메소드로 HTML DOM 노드를 JSX로 바꿈 -> 리액트를 HTML로 통합
+//https://ko.reactjs.org/docs/strict-mode.html
+// ReactDOM.render(JSX, HTML 위치);
 ReactDOM.render(
+  //StrictMode는 애플리케이션 내의 잠재적 문제를 알아내는 도구
+  //애플리케이션 어디에서든지 strict 모드 활성화 가능
+  //꼭 index.js나 render 함수 안이 아니더라도
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+  // public/index.html의 'root' id 엘리먼트를 App 컴포넌트로 대체
 );
 
 // If you want to start measuring performance in your app, pass a function
